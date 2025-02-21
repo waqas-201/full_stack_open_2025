7e36df44b8f53cf61a3b2d5b3623450407b06be7
511991waqas <waqasvu892@gmail.com>
2025-02-21 21:34:28 +0500

    Exercise 1.1 completed

---
+++ part1/introdemo/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App
+      person={{ name: "Lin Lanying", imageId: "1bX5QH6" }}
+      size={undefined}
+    />
+  </StrictMode>
+);
