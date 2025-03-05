28436b28c8321ca52899c7fa893ac5b60dbcf8be
511991waqas <waqasvu892@gmail.com>
2025-03-05 07:57:14 +0500

    Exercise 1.9 completed

--- part1/unicafe/src/compoenets/Statistics.jsx
+++ part1/unicafe/src/compoenets/Statistics.jsx
@@ -1,6 +1,10 @@
 export const Statistics = ({ good, neutral, bad }) => {
+  if (!good && !bad && !neutral) {
+    return <p> No feedback is given </p>;
+  }
   return (
     <>
+      <h2>Statistics</h2>
       <p>all {good + neutral + bad}</p>
       <p>average {(good + neutral + bad) / 3}</p>
       <p>positive {(good + neutral) / 2}</p>
