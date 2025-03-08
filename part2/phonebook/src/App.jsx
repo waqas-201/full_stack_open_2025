7af8df3ec31ebefbc777ee0f3b0d1885f61e25ea
waqas-201 <waqasvu892@gmail.com>
2025-03-08 07:31:35 +0500

    Exercises 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "18738736187631" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState("");
+  const [search, setSearch] = useState("");
 
-  //
+  let filterdPersons = !search
+    ? persons
+    : persons.filter((p) => {
+        const lowercasepersons = p.name.toLocaleLowerCase();
+        return lowercasepersons.includes(search);
+      });
 
   const checkDuplicate = (value) => {
     const result = persons.find((val) => {
@@ -24,6 +33,7 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: number,
+      id: Number(persons.length + 1),
     };
 
     const result = checkDuplicate(newName);
@@ -39,6 +49,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>
+          filter shown with :{" "}
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </p>
+      </div>
       <form>
         <div>
           name:{" "}
@@ -59,7 +79,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {filterdPersons.map((person) => {
         return (
           <div key={person.name}>
             <p>name: {person.name}</p>
