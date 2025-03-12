ddaed49dff7cb5e216eaee285ced9655596ea557
waqas-201 <waqasvu892@gmail.com>
2025-03-12 07:37:15 +0500

    Exercises 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var morgan = require("morgan");
+
 const app = express();
 const PORT = 3001;
 app.use(express.json());
+app.use(morgan("tiny"));
 
 const phoneBook = [
   {
