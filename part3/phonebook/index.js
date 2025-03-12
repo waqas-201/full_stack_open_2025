fd06882e72f5c8b9a177969dc387a8b80001724f
waqas-201 <waqasvu892@gmail.com>
2025-03-12 06:49:06 +0500

    Exercises 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,6 +29,13 @@ app.get("/api/persons", (request, responce) => {
   responce.send(phoneBook);
 });
 
+app.get("/info", (req, res) => {
+  const reqTime = new Date();
+  console.log(reqTime.toString());
+
+  res.send(`phonebook has info of ${phoneBook.length} peopels  ${reqTime}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
