f8c7be5d1bb4af869ecedd83cd25581833f071b6
waqas-201 <waqasvu892@gmail.com>
2025-03-15 06:47:56 +0500

    Exercises 3.13

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 var morgan = require("morgan");
 var cors = require("cors");
+const phoneBook = require("./phoneBook");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.use(express.json());
 
 morgan.token("postBody", (req, res) => {
@@ -13,82 +15,19 @@ morgan.token("postBody", (req, res) => {
 
 app.use(morgan(":method :url :status - :response-time ms - Body: :postBody"));
 
-const phoneBook = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/api/persons", (request, responce) => {
-  responce.send(phoneBook);
+app.get("/api/persons", async (req, res) => {
+  const data = await phoneBook.find({});
+  if (!data) {
+    return res.json({ error: "failed to find persons " });
+  }
+  return res.json(data);
 });
-
 
 app.get("/info", (req, res) => {
   const reqTime = new Date();
   console.log(reqTime.toString());
 
   res.send(`phonebook has info of ${phoneBook.length} peopels  ${reqTime}`);
-});
-
-app.get("/api/persons/:id", (req, res) => {
-  const { id } = req.params;
-  if (!id) {
-    res.status(400).end();
-  }
-  const person = phoneBook.find((person) => id === person.id);
-  if (!person) {
-    return res.status(404).end();
-  }
-  res.json(person);
-});
-
-app.delete("/api/persons/:id", (req, res) => {
-  const { id } = req.params;
-  if (!id) {
-    res.status(400).end();
-  }
-
-  const filterdPersons = phoneBook.filter((person) => person.id !== id);
-  if (!filterdPersons) {
-    res.status(404).end();
-  }
-
-  res.json(filterdPersons);
-});
-
-app.post("/api/persons/", (req, res) => {
-  const { name, number } = req.body;
-
-  if (!name || !number) {
-    return res.status(400).json({ error: "name and number are missing " });
-  }
-
-  const isPersoneExists = phoneBook.find((person) => name === person.name);
-  if (isPersoneExists) {
-    return res.status(400).json({ error: "name already Exists" });
-  }
-  const id = Math.floor(Math.random(1000000) * 10000);
-  const person = { id: String(id), name, number };
-  const result = phoneBook.concat(person);
-  res.json(result);
 });
 
 
