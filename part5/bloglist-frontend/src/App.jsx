6ea7617e7dbc065e3f6e092111cf4dbaf8651795
waqas-201 <waqasvu892@gmail.com>
2025-04-05 21:08:24 +0500

    Exercise 5.6

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import Notification from "./components/Notification";
+import CreateBlog from "./components/CreateBlog";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -10,178 +10,108 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const [user, setUser] = useState(null);
   // blog state
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [likes, setLikes] = useState(0);
-   const [showNotification, setShowNotification] = useState(false);
-   const [message, setMessage] = useState("");
-   const [type, setType] = useState("");
-   const [showCreate, setShowCreate] = useState(false);
 
-   useEffect(() => {
-     blogService.getAll().then((blogs) => setBlogs(blogs));
-   }, [user]);
+  const [showNotification, setShowNotification] = useState(false);
+  const [message, setMessage] = useState("");
+  const [type, setType] = useState("");
 
-   useEffect(() => {
-     const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
-     if (loggedUserJSON) {
-       const user = JSON.parse(loggedUserJSON);
-       setUser(user);
-       blogService.setToken(user.token);
-     }
-   }, []);
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, [user]);
 
-   const handleLogin = async (e) => {
-     e.preventDefault();
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
 
-     try {
-       const user = await blogService.login({ username, password });
-       window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
-       blogService.setToken(user.token);
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-       setUser(user);
+    try {
+      const user = await blogService.login({ username, password });
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+      blogService.setToken(user.token);
 
-       setShowNotification(true);
-       setMessage("user logged in succcessfully");
-       setType("success");
-       // Hide notification after 3 seconds
-       setTimeout(() => {
-         setShowNotification(false);
-       }, 3000);
-       setUsername("");
-       setPassword("");
-     } catch (error) {
-       setShowNotification(true);
-       setTimeout(() => {
-         setShowNotification(false);
-       }, 3000);
-       setMessage(error.response.data.error || error.message);
-       setType("failed");
-       setTimeout(() => {
-         setErrorMessage(null);
-       }, 5000);
-     }
-   };
+      setUser(user);
 
-   const handleCreateBlog = async (e) => {
-     e.preventDefault();
-     try {
-       const responce = await blogService.create({ title, author, likes });
-       setBlogs(blogs.concat(responce));
-       setTitle("");
-       setAuthor("");
-       setLikes("");
-       setShowNotification(true);
-       setTimeout(() => {
-         setShowNotification(false);
-       }, 3000);
+      setShowNotification(true);
+      setMessage("user logged in succcessfully");
+      setType("success");
+      // Hide notification after 3 seconds
+      setTimeout(() => {
+        setShowNotification(false);
+      }, 3000);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setShowNotification(true);
+      setTimeout(() => {
+        setShowNotification(false);
+      }, 3000);
+      setMessage(error.response.data.error || error.message);
+      setType("failed");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
 
-       setMessage(`blog post with title ${responce.title} just  added`);
-       setType("success");
-     } catch (error) {
-       setShowNotification(true);
-       setTimeout(() => {
-         setShowNotification(false);
-       }, 3000);
+  return (
+    <div>
+      {errorMessage ? <p style={{ color: "red" }}>{errorMessage}</p> : null}
+      {showNotification && <Notification message={message} type={type} />}
 
-       setMessage(error?.response?.data?.error);
-       setType("failed");
-       console.log(error);
-     }
-   };
+      {user ? (
+        <>
+          <p> username {user?.username}</p>
+          <button
+            onClick={() => {
+              localStorage.setItem("loggedNoteappUser", "");
+              setUser(null);
+            }}
+          >
+            logout
+          </button>
+        </>
+      ) : null}
 
-   return (
-     <div>
-       {errorMessage ? <p style={{ color: "red" }}>{errorMessage}</p> : null}
-       {showNotification && <Notification message={message} type={type} />}
-
-       {user ? (
-         <>
-           <p> username {user?.username}</p>
-           <button
-             onClick={() => {
-               localStorage.setItem("loggedNoteappUser", "");
-               setUser(null);
-             }}
-           >
-             logout
-           </button>
-         </>
-       ) : null}
-
-       {!user ? (
-         <form onSubmit={(e) => handleLogin(e)}>
-           <div>
-             username
-             <input
-               type="text"
-               value={username}
-               name="Username"
-               onChange={({ target }) => setUsername(target.value)}
-             />
-           </div>
-           <div>
-             password
-             <input
-               type="password"
-               value={password}
-               name="Password"
-               onChange={({ target }) => setPassword(target.value)}
-             />
-           </div>
-           <button type="submit">login</button>
-         </form>
-       ) : (
-         <>
-           <h2>Blogs</h2>
-           {blogs.map((blog) => (
-             <Blog key={blog.id} blog={blog} />
-           ))}
-
-           <button
-             onClick={() => {
-               setShowCreate(!showCreate);
-             }}
-           >
-             create New{" "}
-           </button>
-
-           {showCreate ? (
-             <form onSubmit={(e) => handleCreateBlog(e)}>
-               <div>
-                 title
-                 <input
-                   type="text"
-                   value={title}
-                   name="title"
-                   onChange={(e) => setTitle(e.target.value)}
-                 />
-               </div>
-               <div>
-                 author
-                 <input
-                   type="text"
-                   value={author}
-                   name="author"
-                   onChange={(e) => setAuthor(e.target.value)}
-                 />
-               </div>
-               <div>
-                 likes
-                 <input
-                   type="number"
-                   value={likes}
-                   name="likes"
-                   onChange={(e) => setLikes(e.target.value)}
-                 />
-               </div>
-               <button type="submit">create</button>
-             </form>
-           ) : null}
-         </>
-       )}
-     </div>
-   );
+      {!user ? (
+        <form onSubmit={(e) => handleLogin(e)}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      ) : (
+        <CreateBlog
+          blogs={blogs}
+          setBlogs={setBlogs}
+          setShowNotification={setShowNotification}
+          setMessage={setMessage}
+          setType={setType}
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ part5/bloglist-frontend/src/components/CreateBlog.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Blog from "./Blog";
+import blogService from "../services/blogs";
+
+const CreateBlog = ({
+  blogs,
+  setBlogs,
+  setShowNotification,
+  setMessage,
+  setType,
+}) => {
+  const [showCreate, setShowCreate] = useState(false);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [likes, setLikes] = useState(0);
+
+  const handleCreateBlog = async (e) => {
+    e.preventDefault();
+    try {
+      const responce = await blogService.create({ title, author, likes });
+      setBlogs(blogs.concat(responce));
+      setTitle("");
+      setAuthor("");
+      setLikes("");
+      setShowNotification(true);
+      setTimeout(() => {
+        setShowNotification(false);
+      }, 3000);
+
+      setMessage(`blog post with title ${responce.title} just  added`);
+      setType("success");
+    } catch (error) {
+      setShowNotification(true);
+      setTimeout(() => {
+        setShowNotification(false);
+      }, 3000);
+
+      setMessage(error?.response?.data?.error);
+      setType("failed");
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <h2>Blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+
+      <button
+        onClick={() => {
+          setShowCreate(!showCreate);
+        }}
+      >
+        create New{" "}
+      </button>
+
+      {showCreate ? (
+        <form onSubmit={(e) => handleCreateBlog(e)}>
+          <div>
+            title
+            <input
+              type="text"
+              value={title}
+              name="title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="text"
+              value={author}
+              name="author"
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </div>
+          <div>
+            likes
+            <input
+              type="number"
+              value={likes}
+              name="likes"
+              onChange={(e) => setLikes(e.target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      ) : null}
+    </>
+  );
+};
+
+export default CreateBlog;
